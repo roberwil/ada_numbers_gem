983962299565183ae18b6b6363a259d3971d97c2
Roberto Ribeiro <rober.will@hotmail.com>
2023-03-26 23:46:00 +0100

    - more methods yay, they all serve to convert

--- src/lib/ada_numbers/converters/pt/number_to_words_converter.rb
+++ src/lib/ada_numbers/converters/pt/number_to_words_converter.rb
@@ -53,15 +53,63 @@ module AdaNumbers
         return result
       end
 
-      def self.resolve_number(number, flag = false)
-
-      end
-
-
       private
       def self.select_scale
         @@use_short_scale = Settings.scale == Settings::Parameters::SCALES[:short]
       end
+
+      def self.resolve_number(number, flag = false)
+      end
+
+      def add_separators_to_number(number_tokens)
+        result = number_tokens.first
+
+        (1...number_tokens.size).each do |cursor|
+          current_token = number_tokens[cursor]
+
+          if WrittenNumber::Pt.numbers_that_ignore_separator.include? current_token
+            result += " #{current_token}"
+          else
+            result += " #{Separator::Pt::NUMBERS_SEPARATOR.downcase} #{current_token}"
+          end
+        end
+
+        return result
+      end
+
+      def unities(number)
+        WrittenNumber::Pt.numbers_to_words_map_unities.resolve number
+      end
+
+      def tens(number)
+        WrittenNumber::Pt.numbers_to_words_map_tens.resolve number
+      end
+
+      def hundreds(number, is_cent = false)
+        results = {
+          200 => WrittenNumber::Pt::TWO_HUNDRED,
+          300 => WrittenNumber::Pt::THREE_HUNDRED,
+          400 => WrittenNumber::Pt::FOUR_HUNDRED,
+          500 => WrittenNumber::Pt::FIVE_HUNDRED,
+          600 => WrittenNumber::Pt::SIX_HUNDRED,
+          700 => WrittenNumber::Pt::SEVEN_HUNDRED,
+          800 => WrittenNumber::Pt::EIGHT_HUNDRED,
+          900 => WrittenNumber::Pt::NINE_HUNDRED,
+          100 => (is_cent ? WrittenNumber::Pt::ONE_HUNDRED : WrittenNumber::Pt::ONE_HUNDRED_SINGLE)
+        }
+
+        return results.resolve number
+      end
+
+      def evaluate_thousands_and_over(number, category_identifier, singular, plural)
+        return singular if number == category_identifier
+        return '' if number%category_identifier != 0
+
+        partial_number = number.to_s[0...number.bridge].to_i
+        resolve_number partial_number
+        return plural
+      end
+
     end
   end
 end

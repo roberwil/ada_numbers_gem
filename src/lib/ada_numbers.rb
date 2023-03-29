f49e8820575c22f7c2ef9770d62265a9189b8a97
Roberto Ribeiro <rober.will@hotmail.com>
2023-03-29 20:48:55 +0100

    - setting up tests for number to words converter - english version

--- src/lib/ada_numbers.rb
+++ src/lib/ada_numbers.rb
@@ -2,6 +2,7 @@ require 'ada_numbers/version'
 
 require 'ada_numbers/converters/number_to_words_converter_extensions'
 require 'ada_numbers/converters/pt/number_to_words_converter'
+require 'ada_numbers/converters/en/number_to_words_converter'
 
 require 'ada_numbers/converters/words_to_number_converter_extensions'
 require 'ada_numbers/converters/pt/words_to_number_converter'
--- src/test/number_to_words_converter_en_test.rb
+++ src/test/number_to_words_converter_en_test.rb
@@ -1,6 +1,6 @@
 require "test_helper"
 
-class NumbersToWordsConverterPtTest < Minitest::Test
+class NumbersToWordsConverterEnTest < Minitest::Test
   def self.select_scale(scale)
     AdaNumbers::Settings.scale = AdaNumbers::Settings::Parameters::SCALES[scale]
   end
@@ -9,15 +9,11 @@ class NumbersToWordsConverterPtTest < Minitest::Test
     return set.first, set.last
   end
 
-  def test_that_it_has_a_version_number
-    refute_nil ::AdaNumbers::VERSION
-  end
-
   describe "Sample" do
     [
 
     ].each do |test_set|
-      to_convert, expected = NumbersToWordsConverterPtTest.explode test_set
+      to_convert, expected = NumbersToWordsConverterEnTest.explode test_set
 
       it "#{to_convert} should be '#{expected}'" do
         actual = to_convert.to_w
@@ -26,7 +22,7 @@ class NumbersToWordsConverterPtTest < Minitest::Test
     end
   end
 
-  describe "PT :: To words - Unities are valid" do
+  describe "EN :: To words - Unities are valid" do
     [
       [0, "Zero"],
       [1, "Um"],
@@ -39,7 +35,7 @@ class NumbersToWordsConverterPtTest < Minitest::Test
       [8, "Oito"],
       [9, "Nove"]
     ].each do |test_set|
-      to_convert, expected = NumbersToWordsConverterPtTest.explode test_set
+      to_convert, expected = NumbersToWordsConverterEnTest.explode test_set
 
       it "#{to_convert} should be '#{expected}'" do
         actual = to_convert.to_w
@@ -48,93 +44,93 @@ class NumbersToWordsConverterPtTest < Minitest::Test
     end
   end
 
-  describe "PT :: To words - Tens are valid" do
-      [
-        [10, "Dez"],
-        [11, "Onze"],
-        [12, "Doze"],
-        [13, "Treze"],
-        [14, "Catorze"],
-        [15, "Quinze"],
-        [16, "Dezasseis"],
-        [17, "Dezassete"],
-        [18, "Dezoito"],
-        [19, "Dezanove"],
-        [20, "Vinte"],
-        [21, "Vinte e Um"],
-        [30, "Trinta"],
-        [32, "Trinta e Dois"],
-        [40, "Quarenta"],
-        [43, "Quarenta e Três"],
-        [50, "Cinquenta"],
-        [54, "Cinquenta e Quatro"],
-        [60, "Sessenta"],
-        [65, "Sessenta e Cinco"],
-        [70, "Setenta"],
-        [76, "Setenta e Seis"],
-        [80, "Oitenta"],
-        [87, "Oitenta e Sete"],
-        [90, "Noventa"],
-        [98, "Noventa e Oito"]
-      ].each do |test_set|
-        to_convert, expected = NumbersToWordsConverterPtTest.explode test_set
+  describe "EN :: To words - Tens are valid" do
+    [
+      [10, "Dez"],
+      [11, "Onze"],
+      [12, "Doze"],
+      [13, "Treze"],
+      [14, "Catorze"],
+      [15, "Quinze"],
+      [16, "Dezasseis"],
+      [17, "Dezassete"],
+      [18, "Dezoito"],
+      [19, "Dezanove"],
+      [20, "Vinte"],
+      [21, "Vinte e Um"],
+      [30, "Trinta"],
+      [32, "Trinta e Dois"],
+      [40, "Quarenta"],
+      [43, "Quarenta e Três"],
+      [50, "Cinquenta"],
+      [54, "Cinquenta e Quatro"],
+      [60, "Sessenta"],
+      [65, "Sessenta e Cinco"],
+      [70, "Setenta"],
+      [76, "Setenta e Seis"],
+      [80, "Oitenta"],
+      [87, "Oitenta e Sete"],
+      [90, "Noventa"],
+      [98, "Noventa e Oito"]
+    ].each do |test_set|
+      to_convert, expected = NumbersToWordsConverterEnTest.explode test_set
 
-        it "#{to_convert} should be '#{expected}'" do
-          actual = to_convert.to_w
-          assert_equal expected, actual
-        end
+      it "#{to_convert} should be '#{expected}'" do
+        actual = to_convert.to_w
+        assert_equal expected, actual
       end
     end
-
-  describe "PT :: To words - Hundred are valid" do
-      [
-        [100, "Cem" ],
-        [101, "Cento e Um" ],
-        [111, "Cento e Onze" ],
-        [121, "Cento e Vinte e Um" ],
-        [200, "Duzentos" ],
-        [202, "Duzentos e Dois" ],
-        [212, "Duzentos e Doze" ],
-        [222, "Duzentos e Vinte e Dois" ],
-        [300, "Trezentos" ],
-        [303, "Trezentos e Três" ],
-        [313, "Trezentos e Treze" ],
-        [333, "Trezentos e Trinta e Três" ],
-        [400, "Quatrocentos" ],
-        [404, "Quatrocentos e Quatro" ],
-        [414, "Quatrocentos e Catorze" ],
-        [444, "Quatrocentos e Quarenta e Quatro" ],
-        [500, "Quinhentos" ],
-        [505, "Quinhentos e Cinco" ],
-        [515, "Quinhentos e Quinze" ],
-        [555, "Quinhentos e Cinquenta e Cinco" ],
-        [600, "Seiscentos" ],
-        [606, "Seiscentos e Seis" ],
-        [616, "Seiscentos e Dezasseis" ],
-        [666, "Seiscentos e Sessenta e Seis" ],
-        [700, "Setecentos" ],
-        [707, "Setecentos e Sete" ],
-        [717, "Setecentos e Dezassete" ],
-        [777, "Setecentos e Setenta e Sete" ],
-        [800, "Oitocentos" ],
-        [808, "Oitocentos e Oito" ],
-        [818, "Oitocentos e Dezoito" ],
-        [888, "Oitocentos e Oitenta e Oito" ],
-        [900, "Novecentos" ],
-        [909, "Novecentos e Nove" ],
-        [919, "Novecentos e Dezanove" ],
-        [999, "Novecentos e Noventa e Nove" ]
-      ].each do |test_set|
-        to_convert, expected = NumbersToWordsConverterPtTest.explode test_set
-
-        it "#{to_convert} should be '#{expected}'" do
-          actual = to_convert.to_w
-          assert_equal expected, actual
-        end
-      end
   end
 
-  describe "PT :: To words - Thousands are valid" do
+  describe "EN :: To words - Hundred are valid" do
+    [
+      [100, "Cem" ],
+      [101, "Cento e Um" ],
+      [111, "Cento e Onze" ],
+      [121, "Cento e Vinte e Um" ],
+      [200, "Duzentos" ],
+      [202, "Duzentos e Dois" ],
+      [212, "Duzentos e Doze" ],
+      [222, "Duzentos e Vinte e Dois" ],
+      [300, "Trezentos" ],
+      [303, "Trezentos e Três" ],
+      [313, "Trezentos e Treze" ],
+      [333, "Trezentos e Trinta e Três" ],
+      [400, "Quatrocentos" ],
+      [404, "Quatrocentos e Quatro" ],
+      [414, "Quatrocentos e Catorze" ],
+      [444, "Quatrocentos e Quarenta e Quatro" ],
+      [500, "Quinhentos" ],
+      [505, "Quinhentos e Cinco" ],
+      [515, "Quinhentos e Quinze" ],
+      [555, "Quinhentos e Cinquenta e Cinco" ],
+      [600, "Seiscentos" ],
+      [606, "Seiscentos e Seis" ],
+      [616, "Seiscentos e Dezasseis" ],
+      [666, "Seiscentos e Sessenta e Seis" ],
+      [700, "Setecentos" ],
+      [707, "Setecentos e Sete" ],
+      [717, "Setecentos e Dezassete" ],
+      [777, "Setecentos e Setenta e Sete" ],
+      [800, "Oitocentos" ],
+      [808, "Oitocentos e Oito" ],
+      [818, "Oitocentos e Dezoito" ],
+      [888, "Oitocentos e Oitenta e Oito" ],
+      [900, "Novecentos" ],
+      [909, "Novecentos e Nove" ],
+      [919, "Novecentos e Dezanove" ],
+      [999, "Novecentos e Noventa e Nove" ]
+    ].each do |test_set|
+      to_convert, expected = NumbersToWordsConverterEnTest.explode test_set
+
+      it "#{to_convert} should be '#{expected}'" do
+        actual = to_convert.to_w
+        assert_equal expected, actual
+      end
+    end
+  end
+
+  describe "EN :: To words - Thousands are valid" do
     [
       [1000, "Mil"],
       [1001, "Mil e Um"],
@@ -146,7 +142,7 @@ class NumbersToWordsConverterPtTest < Minitest::Test
       [140000, "Cento e Quarenta Mil"],
       [140001, "Cento e Quarenta Mil e Um"]
     ].each do |test_set|
-      to_convert, expected = NumbersToWordsConverterPtTest.explode test_set
+      to_convert, expected = NumbersToWordsConverterEnTest.explode test_set
 
       it "#{to_convert} should be '#{expected}'" do
         actual = to_convert.to_w
@@ -155,7 +151,7 @@ class NumbersToWordsConverterPtTest < Minitest::Test
     end
   end
 
-  describe "PT :: To words - Millions are valid" do
+  describe "EN :: To words - Millions are valid" do
     [
       [1000000, "Um Milhão"],
       [1000001, "Um Milhão e Um"],
@@ -166,8 +162,8 @@ class NumbersToWordsConverterPtTest < Minitest::Test
       [20000122, "Vinte Milhões Cento e Vinte e Dois"],
       [22000122, "Vinte e Dois Milhões Cento e Vinte e Dois"]
     ].each do |test_set|
-      to_convert, expected = NumbersToWordsConverterPtTest.explode test_set
-      NumbersToWordsConverterPtTest.select_scale :long
+      to_convert, expected = NumbersToWordsConverterEnTest.explode test_set
+      NumbersToWordsConverterEnTest.select_scale :long
 
       it "#{to_convert} should be '#{expected}'" do
         actual = to_convert.to_w
@@ -176,7 +172,7 @@ class NumbersToWordsConverterPtTest < Minitest::Test
     end
   end
 
-  describe "PT :: To words - Thousand Millions are valid" do
+  describe "EN :: To words - Thousand Millions are valid" do
     [
       [1000000000, "Mil Milhões"],
       [1000000001, "Mil Milhões e Um"],
@@ -187,17 +183,17 @@ class NumbersToWordsConverterPtTest < Minitest::Test
       [20000000122, "Vinte Mil Milhões Cento e Vinte e Dois"],
       [22000000122, "Vinte e Dois Mil Milhões Cento e Vinte e Dois"]
     ].each do |test_set|
-      to_convert, expected = NumbersToWordsConverterPtTest.explode test_set
+      to_convert, expected = NumbersToWordsConverterEnTest.explode test_set
 
       it "#{to_convert} should be '#{expected}'" do
-        NumbersToWordsConverterPtTest.select_scale :long
+        NumbersToWordsConverterEnTest.select_scale :long
         actual = to_convert.to_w
         assert_equal expected, actual
       end
     end
   end
 
-  describe "PT :: To words - Thousand Millions in short scale (billions) are valid" do
+  describe "EN :: To words - Thousand Millions in short scale (billions) are valid" do
     [
       [1000000000, "Um Bilião"],
       [1000000001, "Um Bilião e Um"],
@@ -208,17 +204,17 @@ class NumbersToWordsConverterPtTest < Minitest::Test
       [20000000122, "Vinte Biliões Cento e Vinte e Dois"],
       [22000000122, "Vinte e Dois Biliões Cento e Vinte e Dois"]
     ].each do |test_set|
-      to_convert, expected = NumbersToWordsConverterPtTest.explode test_set
+      to_convert, expected = NumbersToWordsConverterEnTest.explode test_set
 
       it "#{to_convert} should be '#{expected}'" do
-        NumbersToWordsConverterPtTest.select_scale :short
+        NumbersToWordsConverterEnTest.select_scale :short
         actual = to_convert.to_w
         assert_equal expected, actual
       end
     end
   end
 
-  describe "PT :: To words - Billions are valid" do
+  describe "EN :: To words - Billions are valid" do
     [
       [1000000000000, "Um Bilião"],
       [1000000000001, "Um Bilião e Um"],
@@ -229,17 +225,17 @@ class NumbersToWordsConverterPtTest < Minitest::Test
       [20000000000122, "Vinte Biliões Cento e Vinte e Dois"],
       [22000000000122, "Vinte e Dois Biliões Cento e Vinte e Dois"]
     ].each do |test_set|
-      to_convert, expected = NumbersToWordsConverterPtTest.explode test_set
+      to_convert, expected = NumbersToWordsConverterEnTest.explode test_set
 
       it "#{to_convert} should be '#{expected}'" do
-        NumbersToWordsConverterPtTest.select_scale :long
+        NumbersToWordsConverterEnTest.select_scale :long
         actual = to_convert.to_w
         assert_equal expected, actual
       end
     end
   end
 
-  describe "PT :: To words - Billions in short scale (trillions) are valid" do
+  describe "EN :: To words - Billions in short scale (trillions) are valid" do
     [
       [1000000000000, "Um Trilião"],
       [1000000000001, "Um Trilião e Um"],
@@ -250,17 +246,17 @@ class NumbersToWordsConverterPtTest < Minitest::Test
       [20000000000122, "Vinte Triliões Cento e Vinte e Dois"],
       [22000000000122, "Vinte e Dois Triliões Cento e Vinte e Dois"],
     ].each do |test_set|
-      to_convert, expected = NumbersToWordsConverterPtTest.explode test_set
+      to_convert, expected = NumbersToWordsConverterEnTest.explode test_set
 
       it "#{to_convert} should be '#{expected}'" do
-        NumbersToWordsConverterPtTest.select_scale :short
+        NumbersToWordsConverterEnTest.select_scale :short
         actual = to_convert.to_w
         assert_equal expected, actual
       end
     end
   end
 
-  describe "PT :: To words - Random integers are valid" do
+  describe "EN :: To words - Random integers are valid" do
     [
       [42, "Quarenta e Dois" ],
       [102, "Cento e Dois" ],
@@ -279,17 +275,17 @@ class NumbersToWordsConverterPtTest < Minitest::Test
       [112123, "Cento e Doze Mil Cento e Vinte e Três" ],
       [134123, "Cento e Trinta e Quatro Mil Cento e Vinte e Três" ]
     ].each do |test_set|
-      to_convert, expected = NumbersToWordsConverterPtTest.explode test_set
+      to_convert, expected = NumbersToWordsConverterEnTest.explode test_set
 
       it "#{to_convert} should be '#{expected}'" do
-        NumbersToWordsConverterPtTest.select_scale :long
+        NumbersToWordsConverterEnTest.select_scale :long
         actual = to_convert.to_w
         assert_equal expected, actual
       end
     end
   end
 
-  describe "PT :: To words - Random decimals are valid" do
+  describe "EN :: To words - Random decimals are valid" do
     [
       [42.2, "Quarenta e Dois vírgula Dois" ],
       [102.0, "Cento e Dois" ],
@@ -300,10 +296,10 @@ class NumbersToWordsConverterPtTest < Minitest::Test
       [100000.001, "Cem Mil vírgula Zero Zero Um" ],
       [100123.100123, "Cem Mil Cento e Vinte e Três vírgula Cem Mil Cento e Vinte e Três" ]
     ].each do |test_set|
-      to_convert, expected = NumbersToWordsConverterPtTest.explode test_set
+      to_convert, expected = NumbersToWordsConverterEnTest.explode test_set
 
       it "#{to_convert} should be '#{expected}'" do
-        NumbersToWordsConverterPtTest.select_scale :long
+        NumbersToWordsConverterEnTest.select_scale :long
         actual = to_convert.to_w
         assert_equal expected, actual
       end
